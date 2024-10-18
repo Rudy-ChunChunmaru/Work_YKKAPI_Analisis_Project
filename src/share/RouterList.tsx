34d3684b3bc52d2283cffde7d9f968ef5220d601
rudymuinfo <rudymuinfo@gmail.com>
2024-10-18 15:10:04 +0000

    add id to router

--- src/share/RouterList.tsx
+++ src/share/RouterList.tsx
@@ -1,25 +1,29 @@
 type routerListType = {
+    id:number,
     title:string,
     pageRoute:string,
     page: JSX.Element,
-    childerRoute?:routerListType[]
+    childernRoute?:routerListType[]
 }
 
 import Index_page from "@/pages/common/index";
 import Standar_list from "@/pages/standard_list";
+import React from "react";
 
 const routerList:routerListType[] = [
     {
+        id:1,
         title:"home",
         pageRoute:"/",
-        page: <Index_page/>,
-        childerRoute:undefined
+        page: React.createElement(Index_page),
+        childernRoute:undefined
     },
     {
+        id:2,
         title:"standard list",
         pageRoute:"/standardlist",
-        page: <Standar_list/>,
-        childerRoute:undefined
+        page: React.createElement(Standar_list),
+        childernRoute:undefined
     }
 ]
 export default routerList;
